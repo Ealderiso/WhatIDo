247344624f245ebd1d63e6f4550462daf150d11c
Ealderiso <Ealderiso@gmail.com>
2015-12-03 22:14:22 -0500

    Initial Commit
    Testing Git Commit

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+//Erik Alderiso 12/3/2015 3434
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        WinEventDelegate dele = null;
+
+        List<apps> ActAppList = new List<apps>();
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            dele = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, dele, 0, 0, WINEVENT_OUTOFCONTEXT);
+
+        }
+
+        delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        private const uint WINEVENT_OUTOFCONTEXT = 0;
+        private const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        private string GetActiveWindowTitle()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return Buff.ToString();
+            }
+            return null;
+        }
+
+        public void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            label1.Text += GetActiveWindowTitle() + "\r\n";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+    }
+}
---
+++ apps.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace WindowsFormsApplication1
+{
+    //https://msdn.microsoft.com/en-us/library/6sh2ey19(v=vs.110).aspx?cs-save-lang=1&cs-lang=csharp#code-snippet-1
+
+    internal class apps : IEquatable<apps>
+    {
+        public string AppName { get; set; }
+        public int AppID { get; set; }
+
+        public override string ToString()
+        {
+            return AppName;
+        }
+
+        public bool Equals(apps other)
+        {
+            if (other == null) return false;
+            return (this.AppID.Equals(other.AppID));
+        }
+    }
+}
